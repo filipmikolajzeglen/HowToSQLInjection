220dd43a24dbaa2846a01c8fcefef193b6c52d8d
Kratos <filipmikolajgabriel@gmail.com>
2020-07-25 23:45:16 +0200

    App protected by PreparedStatment

--- src/pl/filipzeglen/injection/servlet/LoginServlet.java
+++ src/pl/filipzeglen/injection/servlet/LoginServlet.java
@@ -2,8 +2,8 @@ package pl.filipzeglen.injection.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -19,8 +19,8 @@ import javax.sql.DataSource;
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         DataSource ds = null;
         try {
             ds = getDataSource();
@@ -28,27 +28,26 @@ public class LoginServlet extends HttpServlet {
             e.printStackTrace();
             response.sendError(500);
         }
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        // pass2" OR '1'='1'; --
-        final String sqlQuery = buildQuery(username, password);
-        System.out.println(sqlQuery);
 
+        final String sqlQuery = "SELECT username, password FROM user WHERE username=? AND password=?";
         try (Connection conn = ds.getConnection();
-             Statement statement = conn.createStatement();
-             ResultSet resultSet = statement.executeQuery(sqlQuery);
-        ){
-            if(resultSet.next()) {
+             PreparedStatement statement = conn.prepareStatement(sqlQuery);) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
                 String userFound = resultSet.getString("username");
                 request.getSession().setAttribute("username", userFound);
-                if("admin".equals(userFound)) {
-                    request.getSession().setAttribute("priviliges", "all");
-                } else{
-                    request.getSession().setAttribute("priviliges", "view");
+                if ("admin".equals(userFound)) {
+                    request.getSession().setAttribute("privigiles", "all");
+                } else {
+                    request.getSession().setAttribute("privigiles", "view");
                 }
             } else {
                 request.getSession().setAttribute("username", "Nieznajomy");
-                request.getSession().setAttribute("priviliges", "none");
+                request.getSession().setAttribute("privigiles", "none");
             }
             request.getRequestDispatcher("result.jsp").forward(request, response);
         } catch (Exception e) {
@@ -59,17 +58,8 @@ public class LoginServlet extends HttpServlet {
 
     private DataSource getDataSource() throws NamingException {
         Context initialContext = new InitialContext();
-        Context envContext = (Context) initialContext
-                .lookup("java:comp/env");
+        Context envContext = (Context) initialContext.lookup("java:comp/env");
         return (DataSource) envContext.lookup("jdbc/users");
-    }
-
-    private String buildQuery(String username, String password) {
-        // pass2" OR '1'='1'; --
-        return "SELECT username, password FROM user WHERE "
-                +"username=" + "\"" + username + "\" "
-                +"AND "
-                +"password=" + "\"" + password + "\";";
     }
 
 }
